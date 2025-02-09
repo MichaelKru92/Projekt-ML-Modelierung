6858a43f7902b1a8e8f2d073068c3240143ce9cf
MichaelKru92 <michael.kruszewski@master-bids.de>
2025-02-09 19:42:14 +0100

    Update Procfile for Gunicorn binding and modify Flask app to use environment variable for port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template_string
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -47,4 +48,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
